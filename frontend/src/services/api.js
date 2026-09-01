85d26e5c00bca3fe4eddfb4c70fa0d12ceafb35f
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-09-01 23:02:34 +0530

    Update API configuration for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://bangalore-pincode-explorer-sp0z.onrender.com/api'
 })
 
 export const searchPincode = async (pincode) => {
