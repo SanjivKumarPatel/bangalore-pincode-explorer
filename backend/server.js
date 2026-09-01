3239fb694c308a953246bb1788e78dcd486d626b
SanjivKumarPatel <sanjivpatel00001@gmail.com>
2026-09-01 15:59:33 +0530

    Initial Setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+/**
+ * @desc Check if the API server is running
+ * @route GET /
+ * @access Public
+ */
+
+app.get('/', (req, res) => {
+  res.status(200).json({ success: true, message: 'Bangalore Pincode Explorer API is running'})
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`)
+})
